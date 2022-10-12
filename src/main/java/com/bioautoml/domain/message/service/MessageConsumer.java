c2db17d90e4431883bbdac8a8557be1ba73de780
Felipe Cesar Rampazzo <ff.cc.ss.rr@hotmail.com>
2022-10-12 00:48:35 -0300

    Ajustes

--- src/main/java/com/bioautoml/domain/message/service/MessageConsumer.java
+++ src/main/java/com/bioautoml/domain/message/service/MessageConsumer.java
@@ -1,15 +1,10 @@
 package com.bioautoml.domain.message.service;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
 
-    @RabbitListener(queues = {"${application.rabbit.queues.process.dna-rna}", "${application.rabbit.queues.results.generate}"})
-    public void receive(@Payload String fileBody) {
-        System.out.println("Message= " + fileBody);
-    }
+
 
 }
--- src/main/java/com/bioautoml/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/bioautoml/handlers/GlobalExceptionHandler.java
@@ -1,20 +1,25 @@
 package com.bioautoml.handlers;
 
 import com.bioautoml.exceptions.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ExceptionResponse> handleAllException(Exception e){
         ExceptionResponse exceptionResponse = new ExceptionResponse(LocalDateTime.now(), e.getMessage());
-        e.printStackTrace();
+        logger.error(Arrays.toString(e.getStackTrace()));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exceptionResponse);
     }
 
