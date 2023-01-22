dbbb7f7488b364e6f06e28e0ffec066be35ec666
Felipe Cesar Rampazzo <ff.cc.ss.rr@hotmail.com>
2023-01-22 15:20:03 -0300

    Desabilitando testes

--- src/test/java/com/bioautoml/BioautomlApplicationTests.java
+++ src/test/java/com/bioautoml/BioautomlApplicationTests.java
@@ -1,11 +1,13 @@
 package com.bioautoml;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BioautomlApplicationTests {
 
+	@Disabled
 	@Test
 	void contextLoads() {
 	}
