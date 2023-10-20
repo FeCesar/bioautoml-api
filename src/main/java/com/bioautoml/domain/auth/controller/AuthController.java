8ccc67dd4fbcdda0c8525275364ec4d4f2694e81
Octavio.Souza <oivatco123@gmail.com>
2023-10-19 22:23:30 -0300

    "Alterações feitas nos controllers"

--- src/main/java/com/bioautoml/domain/auth/controller/AuthController.java
+++ src/main/java/com/bioautoml/domain/auth/controller/AuthController.java
@@ -3,14 +3,14 @@ package com.bioautoml.domain.auth.controller;
 import com.bioautoml.domain.auth.dto.AuthDTO;
 import com.bioautoml.domain.auth.form.AuthForm;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 public interface AuthController {
 
