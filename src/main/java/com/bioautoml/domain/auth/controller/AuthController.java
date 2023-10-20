edf6f0c91b1da8dfbf624056e9badca6cadbf9d2
Octavio.Souza <oivatco123@gmail.com>
2023-10-19 21:53:16 -0300

    "Alterações feitas nos controllers"

--- src/main/java/com/bioautoml/domain/auth/controller/AuthController.java
+++ src/main/java/com/bioautoml/domain/auth/controller/AuthController.java
@@ -2,24 +2,18 @@ package com.bioautoml.domain.auth.controller;
 
 import com.bioautoml.domain.auth.dto.AuthDTO;
 import com.bioautoml.domain.auth.form.AuthForm;
-import com.bioautoml.domain.auth.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
-public class AuthController {
+public interface AuthController {
 
-    @Autowired
-    private AuthService authService;
-
-    @PostMapping("/")
-    public ResponseEntity<AuthDTO> login(@RequestBody @Valid AuthForm authForm){
-        return ResponseEntity.status(HttpStatus.OK).body(this.authService.login(authForm));
-    }
-
+    @PostMapping
+    ResponseEntity<AuthDTO> login(@RequestBody @Valid AuthForm authForm);
 }
---
+++ src/main/java/com/bioautoml/domain/auth/controller/impl/AuthControllerImpl.java
@@ -0,0 +1,25 @@
+package com.bioautoml.domain.auth.controller.impl;
+
+import com.bioautoml.domain.auth.controller.AuthController;
+import com.bioautoml.domain.auth.dto.AuthDTO;
+import com.bioautoml.domain.auth.form.AuthForm;
+import com.bioautoml.domain.auth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Component
+public class AuthControllerImpl implements AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Override
+    @CrossOrigin(value = "http://localhost:3000")
+    public ResponseEntity<AuthDTO> login(AuthForm authForm) {
+        return ResponseEntity.status(HttpStatus.OK).body(this.authService.login(authForm));
+    }
+
+}
