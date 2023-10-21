a50cb8b309372001277e95875985e2b36946e5ba
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2023-10-21 13:04:04 -0300

    bugfix

--- src/main/java/com/bioautoml/domain/process/model/ProcessModel.java
+++ src/main/java/com/bioautoml/domain/process/model/ProcessModel.java
@@ -25,6 +25,7 @@ import java.util.List;
 public class ProcessModel implements BaseEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
