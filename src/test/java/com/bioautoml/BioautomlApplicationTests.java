f15e4aff04a64e29ace0538d56c7926dbf3cfdbb
Felipe Cesar Sepulvida Rampazzo <ff.cc.ss.rr@gmail.com>
2024-02-20 21:07:13 -0300

    Compondo docker-compose

--- src/test/java/com/bioautoml/BioautomlApplicationTests.java
+++ src/test/java/com/bioautoml/BioautomlApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class BioautomlApplicationTests {
 
-	@Disabled
 	@Test
 	void contextLoads() {
 	}
